2526b10425ac5ebaac9f68e1e89c4180b1fa93d4
Philipp Mossier <mossier.dev@gmail.com>
2020-08-14 23:41:23 +0200

    add zsh syntax highlithing

--- mustHaves.sh
+++ mustHaves.sh
@@ -163,6 +163,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 
 echo ""
 echo "##################################################"
+echo "########### zsh-syntax-highlighting ##############"
+echo "##################################################"
+echo ""
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+
+echo ""
+echo "##################################################"
 echo "#################### tree ########################"
 echo "##################################################"
 echo ""
