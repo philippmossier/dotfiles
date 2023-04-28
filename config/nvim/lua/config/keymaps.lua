874cc78608f3f1bb752d0ae86e81e751d5b0922f
philippmossier <mossier.dev@gmail.com>
2023-04-28 12:42:27 +0200

    add jj to enter normal mode

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here:
 
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("i", "jj", "<esc>", { desc = "enter normal mode" })
 -- ###################################################################
 -- ### Current default from lazyvim project (28.04.2023)           ###
 -- ### This file is automatically loaded by lazyvim.plugins.config ###
