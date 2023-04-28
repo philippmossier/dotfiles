fb742af09b4fab8e28b5299c873e2dbb1a6e6c16
philippmossier <mossier.dev@gmail.com>
2023-04-28 12:09:51 +0200

    add hop plugin

--- config/nvim/lua/plugins/general.lua
+++ config/nvim/lua/plugins/general.lua
@@ -32,4 +32,12 @@ return {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+  {
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+    end,
+  },
 }
