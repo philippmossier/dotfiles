935081d27b35659f73a1b01b575556acfcfcb03f
philippmossier <mossier.dev@gmail.com>
2023-05-13 00:40:12 +0200

    cleanup tailwind lsp related config for monorepo and non monorepo

--- config/nvim/lua/plugins/tailwind.lua
+++ config/nvim/lua/plugins/tailwind.lua
@@ -1,7 +1,3 @@
--- if true then
---   return {}
--- end
---
 local util = require("lspconfig.util")
 
 return {
@@ -10,60 +6,22 @@ return {
     opts = {
       servers = {
         tailwindcss = {
-          -- default_config = { root_dir = "/Users/phil/twtest/tailwind.config.ts" },
-          -- experimental = {
-          --   -- configFile = vim.fn.stdpath("config") .. "/tailwind/index.ts",
-          --   -- configFile = "packages/config/tailwind/index.ts",
-          --   -- configFile = "/tailwind/index.ts",
-          --   -- configFile = "/Users/phil/twurbonpm/packages/config/tailwind/index.ts"
-          --   configFile = "/Users/phil/twtest/tailwind.config.ts",
-
-          -- root_dir = { util.root_pattern("tailwind.config.js") },
-          -- root_dir = function(fname)
-          --   return util.root_pattern(
-          --     "/packages/configs/tailwind/tailwind.config.js",
-          --     "packages/configs/tailwind/tailwind.config.ts"
-          --   )(fname) or util.root_pattern(
-          --     "/packages/config/tailwind/tailwind.config.js",
-          --     "packages/config/tailwind/tailwind.config.ts"
-          --   )(fname) or util.root_pattern(
-          --     "/packages/config/tailwind/index.ts",
-          --     "packages/config/tailwind/index.js"
-          --   )(fname) or util.root_pattern(
-          --     "/packages/config/tailwind/postcss.config.ts",
-          --     "packages/config/tailwind/postcss.config.js"
-          --   )(fname) or util.root_pattern("postcss.config.js", "postcss.config.ts")(fname) or util.find_package_json_ancestor(
-          --     fname
-          --   ) or util.find_node_modules_ancestor(fname) or util.find_git_ancestor(fname)
-          -- end,
-
-          -- use this root_dir for t3turbo monorepo and/or with settings.tailwindCss.experimental.configFile = "/Users/phil/lastturbo/packages/config/tailwind/index.ts"
-          root_dir = function(fname) return util.find_git_ancestor(fname) end, -- only use this line for some monorepos
+          -- default values can be found at https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tailwindcss
+          root_dir = util.root_pattern('.git'), -- use this line only for monorepos like t3turbo
           settings = {
             tailwindCSS = {
               experimental = {
-                -- configFile = "/Users/phil/lastturbo/packages/config/tailwind/index.ts",
-                -- configFile = util.root_pattern("config") .. "/packages/config/tailwind/index.ts",
-
-                -- if the root_dir or configFile above does not work for your monorepo: remove root_dir and use just configFile on the next line
-                -- configFile = vim.fn.stdpath("config") .. "/tailwind.config.js", -- put tailwind.config.js into /Users/phil/.config/nvim (sometimes a empty tailwind.config.js is enough)
+                -- configFile = vim.fn.stdpath("config") .. "/tailwind.config.js", -- monorepo option 2 (try this if "root_dir = util.root_pattern('.git')" does not work for monorepos, make sure to put tailwind.config.js to /Users/phil/nvim)
 
                 -- add special classRegex for tailwind to work anywhere
-                classRegex = {
-                  -- twin macro
-                  "tw`([^`]*)",          -- tw`...`
-                  'tw="([^"]*)',         -- <div tw="..." />
-                  'tw={"([^"}]*)',       --  <div tw={"..."} />
-                  "tw\\.\\w+`([^`]*)",   -- tw.xxx`...`
-                  "tw\\(.*?\\)`([^`]*)", -- tw(Component)`...`
-
-                  -- for haml :D
-                  -- "%\\w+([^\\s]*)",
-                  -- "\\.([^\\.]*)",
-                  -- ':class\\s*=>\\s*"([^"]*)',
-                  -- 'class:\\s+"([^"]*)',
-                  --
-                },
+                -- classRegex = {
+                --   -- twin macro
+                --   "tw`([^`]*)",          -- tw`...`
+                --   'tw="([^"]*)',         -- <div tw="..." />
+                --   'tw={"([^"}]*)',       --  <div tw={"..."} />
+                --   "tw\\.\\w+`([^`]*)",   -- tw.xxx`...`
+                --   "tw\\(.*?\\)`([^`]*)", -- tw(Component)`...`
+                -- },
               },
             },
           },
