c482c410172274d586d0764f7a7ae97f5694f9ba
philippmossier <mossier.dev@gmail.com>
2023-05-27 00:45:31 +0200

    remove find plugin to enable find project

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,13 +5,13 @@ return {
       -- add grep(cwd) from <leader>sG also to <leader>F
       { "<leader>F", require("lazyvim.util").telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
       -- add a keymap to browse plugin files
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
+      -- {
+      --   "<leader>fp",
+      --   function()
+      --     require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      --   end,
+      --   desc = "Find Plugin File",
+      -- },
     },
   },
 }
